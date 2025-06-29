81416b1f1ad0bee65af380fb604604190eff375f
Rey <89825425+Kingg22@users.noreply.github.com>
2025-06-29 14:15:33 -0500

    feat(vibrion-app): add crypto stuff and change preferences save token

--- app/src/main/kotlin/io/github/kingg22/vibrion/app/data/datasource/PreferencesDataSource.kt
+++ app/src/main/kotlin/io/github/kingg22/vibrion/app/data/datasource/PreferencesDataSource.kt
@@ -2,9 +2,12 @@ package io.github.kingg22.vibrion.app.data.datasource
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.byteArrayPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
+import io.github.kingg22.vibrion.app.data.Crypto.decrypt
+import io.github.kingg22.vibrion.app.data.Crypto.encrypt
 import io.github.kingg22.vibrion.app.domain.model.AppSettings
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
@@ -13,57 +16,36 @@ import kotlinx.coroutines.flow.mapLatest
 
 data class PreferencesDataSource(private val dataStore: DataStore<Preferences>) {
     companion object {
-        // Search History Keys
-        private val SEARCH_HISTORY_KEY = stringPreferencesKey("search_history")
-
         // Settings Keys
-        private val TOKEN_KEY = stringPreferencesKey("token")
+        private val TOKEN_KEY = byteArrayPreferencesKey("token")
         private val DOWNLOAD_PATH_KEY = stringPreferencesKey("download_path")
         private val MAX_CONCURRENT_DOWNLOADS_KEY = intPreferencesKey("max_concurrent_downloads")
-    }
-
-    // Search History Methods
-    fun getSearchHistory() = dataStore.data.map { preferences ->
-        preferences[SEARCH_HISTORY_KEY]?.split(",")?.filter { it.isNotBlank() } ?: emptyList()
-    }
-
-    suspend fun saveSearchQuery(query: String) {
-        dataStore.edit { preferences ->
-            val history = preferences[SEARCH_HISTORY_KEY]?.split(",")?.toMutableList() ?: mutableListOf()
-            if (!history.contains(query)) {
-                history.add(0, query)
-                if (history.size > 25) history.removeAt(history.lastIndex)
-                preferences[SEARCH_HISTORY_KEY] = history.joinToString(",")
-            }
-        }
-    }
-
-    suspend fun clearSearchHistory() {
-        dataStore.edit { it[SEARCH_HISTORY_KEY] = "" }
     }
 
     // Settings Methods
     fun loadSettings() = dataStore.data.map { preferences ->
         AppSettings(
-            token = preferences[TOKEN_KEY] ?: "",
-            downloadPath = preferences[DOWNLOAD_PATH_KEY] ?: "",
-            maxConcurrentDownloads = preferences[MAX_CONCURRENT_DOWNLOADS_KEY] ?: 3,
+            token = preferences[TOKEN_KEY]?.decrypt(),
+            downloadPath = preferences[DOWNLOAD_PATH_KEY] ?: AppSettings.DEFAULT_DOWNLOAD_PATH,
+            maxConcurrentDownloads =
+            preferences[MAX_CONCURRENT_DOWNLOADS_KEY] ?: AppSettings.DEFAULT_MAX_CONCURRENT_DOWNLOAD,
         )
     }
 
-    fun loadToken() = dataStore.data.map { preferences -> preferences[TOKEN_KEY] }
+    fun loadToken() = dataStore.data.map { preferences -> preferences[TOKEN_KEY]?.decrypt() }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     suspend fun getSettings() = dataStore.data.mapLatest { preferences ->
         AppSettings(
-            token = preferences[TOKEN_KEY] ?: "",
-            downloadPath = preferences[DOWNLOAD_PATH_KEY] ?: "",
-            maxConcurrentDownloads = preferences[MAX_CONCURRENT_DOWNLOADS_KEY] ?: 3,
+            token = preferences[TOKEN_KEY]?.decrypt(),
+            downloadPath = preferences[DOWNLOAD_PATH_KEY] ?: AppSettings.DEFAULT_DOWNLOAD_PATH,
+            maxConcurrentDownloads =
+            preferences[MAX_CONCURRENT_DOWNLOADS_KEY] ?: AppSettings.DEFAULT_MAX_CONCURRENT_DOWNLOAD,
         )
     }.first()
 
     suspend fun updateToken(token: String) {
-        dataStore.edit { it[TOKEN_KEY] = token }
+        dataStore.edit { it[TOKEN_KEY] = token.encrypt() }
     }
 
     suspend fun updateDownloadPath(path: String) {
--- app/src/main/kotlin/io/github/kingg22/vibrion/app/domain/model/AppSettings.kt
+++ app/src/main/kotlin/io/github/kingg22/vibrion/app/domain/model/AppSettings.kt
@@ -1,3 +1,12 @@
 package io.github.kingg22.vibrion.app.domain.model
 
-data class AppSettings(val token: String = "", val downloadPath: String = "", val maxConcurrentDownloads: Int = 1)
+data class AppSettings(
+    val token: String? = null,
+    val downloadPath: String = DEFAULT_DOWNLOAD_PATH,
+    val maxConcurrentDownloads: Int = DEFAULT_MAX_CONCURRENT_DOWNLOAD,
+) {
+    companion object {
+        const val DEFAULT_MAX_CONCURRENT_DOWNLOAD = 3
+        const val DEFAULT_DOWNLOAD_PATH = ""
+    }
+}
