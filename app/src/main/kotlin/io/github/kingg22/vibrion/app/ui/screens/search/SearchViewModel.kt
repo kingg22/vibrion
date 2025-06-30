f7f27e0bd231476a21096bcd8b54f9606b857378
Rey <89825425+Kingg22@users.noreply.github.com>
2025-06-29 23:01:11 -0500

    feat(vibrion-app): add missing file of 1e93a95624004960469b202141083d73ab73df06

--- app/src/main/kotlin/io/github/kingg22/vibrion/app/ui/screens/search/SearchViewModel.kt
+++ app/src/main/kotlin/io/github/kingg22/vibrion/app/ui/screens/search/SearchViewModel.kt
@@ -1,20 +1,52 @@
 package io.github.kingg22.vibrion.app.ui.screens.search
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.github.kingg22.deezerSdk.gw.DeezerGwClient
+import io.github.kingg22.deezerSdk.gw.requests.MediaRequest.Companion.toMediaRequest
+import io.github.kingg22.deezerSdk.utils.HttpClientBuilder
+import io.github.kingg22.deezerSdk.utils.UnofficialDeezerApi
 import io.github.kingg22.vibrion.app.domain.model.Single
 import io.github.kingg22.vibrion.app.domain.repository.SearchRepository
+import io.github.kingg22.vibrion.app.domain.repository.SettingsRepository
+import io.github.kingg22.vibrion.core.application.DownloadForegroundService
+import io.github.kingg22.vibrion.core.application.DownloadOrchestrator
+import io.github.kingg22.vibrion.core.di.AndroidMusicStorage
+import io.github.kingg22.vibrion.core.domain.model.DownloadProvider
+import io.github.kingg22.vibrion.ext.DeezerId3
+import io.github.kingg22.vibrion.ext.toMusicMetadata
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.time.Duration
 
-data class SearchViewModel(private val repository: SearchRepository) : ViewModel() {
+@OptIn(UnofficialDeezerApi::class)
+data class SearchViewModel(
+    private val repository: SearchRepository,
+    private val settingsRepository: SettingsRepository,
+    private val orchestrator: DownloadOrchestrator,
+    private val httpClientBuilder: HttpClientBuilder,
+) : ViewModel() {
+    private val token = settingsRepository.loadToken()
+    private var canDownload: Boolean = false
     private val _searchResults = MutableStateFlow<SearchResultUiState>(SearchResultUiState.Idle)
     val searchResults = _searchResults.asStateFlow()
+    private lateinit var gwClient: DeezerGwClient
 
-    fun canDownload(): Boolean = false
+    init {
+        viewModelScope.launch {
+            token.collect {
+                canDownload = it != null && DeezerGwClient.verifyArl(it, httpClientBuilder.copy())
+            }
+        }
+    }
+
+    fun canDownload() = canDownload
 
     fun search(query: String) {
         _searchResults.update { SearchResultUiState.Loading }
@@ -46,9 +78,27 @@ data class SearchViewModel(private val repository: SearchRepository) : ViewModel
         _searchResults.update { state }
     }
 
-    fun download(item: Single) {
+    fun download(item: Single, context: Context) {
+        if (!canDownload) return
+        checkNotNull(item.id)
         viewModelScope.launch {
-            // TODO
+            token.filterNotNull().collectLatest { arl ->
+                if (!::gwClient.isInitialized) gwClient = DeezerGwClient.initialize(arl, httpClientBuilder.copy())
+                val track = gwClient.tracks.getTrackData(item.id.toLong())()
+                val mediaResult = gwClient.getMedias(track.trackToken.toMediaRequest())
+                val urls = mediaResult.getAllUrls()
+                check(urls.isNotEmpty())
+                DownloadForegroundService.start(context)
+                val download = orchestrator.createDownload(
+                    urls.first(),
+                    item.title,
+                    "vibrion/${item.title}",
+                    DownloadProvider.DeezerId3 + DownloadProvider.AndroidMusicStorage,
+                    DownloadProvider.deezerProcessMetadata(item.id.toLong()),
+                    track.toTrack().toMusicMetadata(),
+                )
+                Log.i("SearchViewModel", "Download enqueue: ${download.getOrNull()}")
+            }
         }
     }
 
