683bc6fd35975b32ccc820c0eef35a71f87faa46
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-22 18:15:55 -0500

    perf(composeApp, android): use androidContext of koin scope

--- composeApp/src/androidMain/kotlin/io/github/kingg22/vibrion/di/DataModule.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/kingg22/vibrion/di/DataModule.android.kt
@@ -1,8 +1,8 @@
 package io.github.kingg22.vibrion.di
 
-import android.content.Context
 import io.ktor.client.plugins.cache.storage.CacheStorage
 import io.ktor.client.plugins.cache.storage.FileStorage
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.scope.Scope
 
-actual fun Scope.cacheDirFor(dir: String): CacheStorage = FileStorage(get<Context>().cacheDir.resolve(dir))
+actual fun Scope.cacheDirFor(dir: String): CacheStorage = FileStorage(androidContext().cacheDir.resolve(dir))
