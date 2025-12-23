b1ee4e726393f71a5c254018bdf95070527ef894
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-22 21:29:16 -0500

    refactor(composeApp, web): remove web files because deezer api is not js friendly CORS

--- composeApp/src/webMain/kotlin/io/github/kingg22/vibrion/di/DataModule.web.kt
+++
@@ -1,6 +0,0 @@
-package io.github.kingg22.vibrion.di
-
-import io.ktor.client.plugins.cache.storage.CacheStorage
-import org.koin.core.scope.Scope
-
-actual fun Scope.cacheDirFor(dir: String): CacheStorage = CacheStorage.Unlimited()
--- composeApp/src/webMain/kotlin/io/github/kingg22/vibrion/di/PlatformModule.web.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kingg22.vibrion.di
-
-import org.koin.core.module.Module
-import org.koin.dsl.module
-
-actual val vibrionPlatformModule: Module
-    get() = module {}
