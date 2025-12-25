851c9080ade0cee3da9724da2a355f83b8e9d7c4
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-24 23:02:43 -0500

    perf(composeApp): move canDownload state flow to mapLatest flow

--- composeApp/src/commonMain/kotlin/io/github/kingg22/vibrion/ui/screens/download/DownloadViewModel.kt
+++ composeApp/src/commonMain/kotlin/io/github/kingg22/vibrion/ui/screens/download/DownloadViewModel.kt
@@ -9,16 +9,16 @@ import io.github.kingg22.vibrion.domain.usecase.settings.LoadTokenUseCase
 import io.sentry.kotlin.multiplatform.Sentry
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -31,43 +31,44 @@ class DownloadViewModel(
         private val logger = Logger.withTag("DownloadViewModel")
     }
     private val token = loadTokenUseCase()
-    private val _canDownloadState = MutableStateFlow<CanDownloadState>(CanDownloadState.Idle)
-    val canDownloadState = _canDownloadState.asStateFlow()
 
-    init {
-        viewModelScope.launch(defaultDispatcher) {
-            token
-                .distinctUntilChanged()
-                .onEach { token ->
-                    logger.d { "Token changed for download." }
-                    if (token == null) {
-                        logger.d { "Token is null, can't download." }
-                        _canDownloadState.update { CanDownloadState.Unavailable }
-                    }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val canDownloadState = token
+        .distinctUntilChanged()
+        .mapLatest { token ->
+            logger.d { "Token changed for download." }
+
+            if (token == null) {
+                logger.d { "Token is null, can't download." }
+                return@mapLatest CanDownloadState.Unavailable
+            }
+
+            // emit(CanDownloadState.Loading)
+
+            try {
+                val canDownload = downloadService.canDownload(token)
+                if (canDownload) {
+                    logger.i { "Can download with current token." }
+                    CanDownloadState.Success
+                } else {
+                    logger.w { "Can't download with the current token" }
+                    CanDownloadState.Unavailable
                 }
-                .filterNotNull()
-                .catch { e ->
-                    logger.e(e) { "Unexpected error while loading token for download." }
-                    _canDownloadState.update { CanDownloadState.Error }
-                }
-                .collect { token ->
-                    _canDownloadState.update { CanDownloadState.Loading }
-                    try {
-                        if (downloadService.canDownload(token)) {
-                            logger.i { "Can download with current token." }
-                            _canDownloadState.update { CanDownloadState.Success }
-                        } else {
-                            logger.w { "Can't download with the current token" }
-                            _canDownloadState.update { CanDownloadState.Unavailable }
-                        }
-                    } catch (e: Exception) {
-                        currentCoroutineContext().ensureActive()
-                        logger.e(e) { "Error while checking if can download." }
-                        _canDownloadState.update { CanDownloadState.Error }
-                    }
-                }
+            } catch (e: Exception) {
+                currentCoroutineContext().ensureActive()
+                logger.e(e) { "Error while checking if can download." }
+                CanDownloadState.Error
+            }
         }
-    }
+        .catch { e ->
+            logger.e(e) { "Unexpected error while loading token for download." }
+            emit(CanDownloadState.Error)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = CanDownloadState.Idle,
+        )
 
     fun download(item: DownloadableItem) {
         /*val transaction = Sentry.startTransaction(
