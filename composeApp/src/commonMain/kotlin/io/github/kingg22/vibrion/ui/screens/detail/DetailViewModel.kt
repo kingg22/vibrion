c7f4a781954f50e6241a3ab0da092fe5932eca65
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-24 23:02:05 -0500

    perf(composeApp): use explicit backing field in detail VM

--- composeApp/src/commonMain/kotlin/io/github/kingg22/vibrion/ui/screens/detail/DetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/io/github/kingg22/vibrion/ui/screens/detail/DetailViewModel.kt
@@ -9,23 +9,23 @@ import io.github.kingg22.vibrion.domain.model.ModelType.GENRE
 import io.github.kingg22.vibrion.domain.model.ModelType.USER
 import io.github.kingg22.vibrion.domain.usecase.search.LoadDetailUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val loadDetailUseCase: LoadDetailUseCase) : ViewModel() {
-    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
-    val uiState = _uiState.asStateFlow()
+    val uiState: StateFlow<UiState>
+        field = MutableStateFlow<UiState>(UiState.Loading)
 
     fun loadDetail(type: ModelType, id: String) {
         if (type == ARTIST || type == USER || type == GENRE) return
         viewModelScope.launch {
-            _uiState.update { UiState.Loading }
+            uiState.update { UiState.Loading }
             val detail = loadDetailUseCase(type, id)
             if (detail != null) {
-                _uiState.update { UiState.Success(detail) }
+                uiState.update { UiState.Success(detail) }
             } else {
-                _uiState.value = UiState.Error
+                uiState.value = UiState.Error
             }
         }
     }
