ae03182c6402ef04458df09d094acfe7ca054f7e
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-17 11:06:12 -0500

    build: update android library DSL with AGP Multiplatform Library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -60,7 +60,7 @@ kotlin {
     // Target declarations - add or remove as needed below. These define
     // which platforms this KMP module supports.
     // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
-    androidLibrary {
+    android {
         namespace = "$group.vibrion.composeApp"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
